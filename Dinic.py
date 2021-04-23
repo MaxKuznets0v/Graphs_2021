df2e1e1fd90cf708c80c376ece39b23878e98464
MaxKuznets0v <max.kuznetsov@yahoo.com>
2021-04-23 14:37:47 +0300

    add dinic algorithm

--- Dinic.py
+++ Dinic.py
@@ -8,6 +8,7 @@ class Graph:
         for i in range(len(self.cap)):
             self.flow.append([0 for i in range(len(self.cap))])
         self.distances = list()
+        self.first_edge = list()
 
     def bfs(self):
         import queue
@@ -23,3 +24,19 @@ class Graph:
                     self.distances[v] = self.distances[cur] + 1
                     q.put(v)
         return self.distances[self.sink] != float('Inf')
+
+    def dfs(self, v, flow):
+        pass
+
+    def dinic(self):
+        maxFlow = 0
+        # пока сток достижим в остаточной сети
+        while self.bfs():
+            self.first_edge = [0 for i in range(len(self.cap))]
+            flow = self.dfs(self.source, float('Inf'))
+            # пока находятся блокирующие пути
+            while flow != 0:
+                maxFlow += flow
+                flow = self.dfs(self.source, float('Inf'))
+
+        return maxFlow
