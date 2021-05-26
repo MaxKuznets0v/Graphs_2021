ca418fa096b3db3a14cdcd34787da93ab64205c1
alinadmb <43730690+alinadmb@users.noreply.github.com>
2021-05-26 23:14:42 +0300

    deleted changes in Dinic

--- Dinic.py
+++ Dinic.py
@@ -17,7 +17,7 @@ class Graph:
         :param sink: индекс вершины - стока
         """
         import copy
-        # self.cap = copy.deepcopy(capacity)
+        self.cap = copy.deepcopy(capacity)
         self.cap = capacity
         self.source = source
         self.sink = sink
